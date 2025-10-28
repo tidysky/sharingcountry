3c0a5ac16676536ed07f8b0d28d0164cd418666a
norazheng-usyd <zhengnaimeng@hotmail.com>
2025-10-28 13:21:17 +1100

    Update cardintro.jsx

--- src/components/cardintro.jsx
+++ src/components/cardintro.jsx
@@ -33,7 +33,7 @@ export default function CardIntro(){
     return(
         <>
         <div className="relative w-screen h-screen overflow-hidden bg-[#333333] z-10 cardIntro flex md:items-center md:justify-center  " >
-             <div className="singlecard z-10 relative flex flex-col items-center md:items-left text-center md:justify-center pr-[0] md:pr-[30%] pt-[10] md:pt-0" data-speed="1.8">
+             <div className="hidden md:block singlecard z-10 relative flex flex-col items-center md:items-left text-center md:justify-center pr-[0] md:pr-[30%] pt-[10] md:pt-0" data-speed="1.8">
                 <motion.p className="text-2xl md:text-3xl font-bold text-[#e3e3e3] py-[1%] opacity-0"
                 whileInView={{
                     x:-20,
@@ -76,6 +76,51 @@ export default function CardIntro(){
                  }}
                  >ELEMENTS</motion.p>
             </div>
+
+            <div className="block md:hidden singlecard z-10 relative flex flex-col items-center text-center justify-center pr-[30%] pt-[0] " data-speed="1.8">
+                <motion.p className="text-3xl font-bold text-[#e3e3e3] py-[1%] opacity-0"
+                whileInView={{
+                    x:-20,
+                    opacity:1,
+                }}
+                transition={{
+                        duration: 0.5, // 动画时长，单位秒
+                        delay: 0.2,  // 延迟时间，单位秒
+                 }}
+                >EACH CARD INCLUDES...</motion.p>
+                <motion.p className="text-5xl font-bold text-[#928c71] eras"
+                whileInView={{
+                    x:20,
+                    opacity:1,
+                }}
+                transition={{
+                        duration: 0.5, // 动画时长，单位秒
+                        delay: 0.2,  // 延迟时间，单位秒
+                 }}
+                 >DIFFERENT</motion.p>
+                <motion.p 
+                whileInView={{
+                    x:-20,
+                    opacity:1,
+                }}
+                transition={{
+                        duration: 0.5, // 动画时长，单位秒
+                        delay: 0.2,  // 延迟时间，单位秒
+                 }}
+                 className="text-4xl font-bold text-[#928c71] eras py-[1%] ">
+                    CULTURE</motion.p>
+                <motion.p className="text-5xl font-bold text-[#928c71] eras"
+                whileInView={{
+                    x:20,
+                    opacity:1,
+                }}
+                transition={{
+                        duration: 0.5, // 动画时长，单位秒
+                        delay: 0.2,  // 延迟时间，单位秒
+                 }}
+                 >ELEMENTS</motion.p>
+            </div>
+
             <div
   className="
     cardimg-singlecard
